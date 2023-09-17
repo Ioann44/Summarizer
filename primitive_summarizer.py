4a2df4801900411108520dee9346ff79fc205ade
Ivan Yashin <borec-ivan@bk.ru>
2023-09-17 12:37:57 +0400

    Working version of primitive summarizer

--- primitive_summarizer.py
+++ primitive_summarizer.py
@@ -25,13 +25,55 @@ assert (
 ), "Get_array_of_counters not working properly"
 
 
+def get_grade_fast(sentence: Counter, total_counter: Counter, total_words) -> float:
+    """Works in O(n), low effect of graded sentence length"""
+    grade = 0
+    for word_key, word_count in sentence.items():
+        grade += word_count * (total_counter[word_key] - word_count) / total_words
+    return grade
+
+
+def get_grade_slow(sentence: Counter, counters: List[Counter]) -> float:
+    """Works in O(n^2), hight effect of graded sentence length"""
+    grade = 0
+    sentence_len = sum(sentence.values())
+    for other_sentence in counters:
+        cur_grade = 0
+        for word_key, word_count in sentence.items():
+            cur_grade += word_count * other_sentence.get(word_key, 0)
+        cur_grade /= sentence_len + sum(other_sentence.values())
+        grade += cur_grade
+    return grade
+
+
 def summarize(text: str):
     sentences = tools.split_to_sentences(text)
     counters = __get_list_of_counters(sentences)
     total_counter = Counter()
+    total_words = 0
     for counter in counters:
         total_counter += counter
+        total_words += sum(counter.values())
+    # choice one of grade functions
+    index_with_grade = [
+        (
+            i,
+            # get_grade_fast(counter, total_counter, total_words)
+            get_grade_slow(counter, counters),
+        )
+        for i, counter in enumerate(counters)
+    ]
+    index_with_grade.sort(key=lambda i_grade: i_grade[1])
+    # lower sentence len in 2 times
+    summarized_index_with_grade = index_with_grade[: len(sentences) // 2]
+    summarized_index_with_grade.sort()
+    return " ".join(sentences[i] for i, _ in summarized_index_with_grade)
 
 
 if __name__ == "__main__":
-    pass
+    with open("other/input.txt", "r", encoding="utf-8") as file:
+        input_text = file.read()
+    res = summarize(input_text)
+    with open("other/output.txt", "a", encoding="utf-8") as file:
+        file.write("\n" * 2)
+        file.write(res)
