ac14f982ea04d08fe3072ffe899b4f65dc9b4382
Ioann44 <borec-ivan@bk.ru>
2024-03-03 18:59:07 +0400

    Move training to laptop

--- summarizer/prelearned_summarizer/model_trainer.py
+++ summarizer/prelearned_summarizer/model_trainer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 from typing import List
 from collections import Counter
 
@@ -26,26 +27,34 @@ class MyDataset:
 
 class TrainingProgressCallback(CallbackAny2Vec):
     def __init__(self):
-        self.prev_loss_total = 0
         self.epoch = 1
+        self.previous_epoch_time = time.time()
 
     def on_epoch_end(self, model):
+        now = time.time()
+        epoch_seconds = now - self.previous_epoch_time
+        self.previous_epoch_time = now
+
         loss = model.get_latest_training_loss()
-        self.prev_loss_total, loss = loss, loss - self.prev_loss_total
-        print("Эпоха #{}: Потери: {}".format(self.epoch, loss))
+        model.running_training_loss = 0.0
+
+        print(f"Эпоха {self.epoch:3}, Потери: {loss}, Время: {epoch_seconds:.2f} с")
         self.epoch += 1
 
 
-def create_model(name, dataset: MyDataset):
+def create_model(name, dataset: MyDataset | None = None):
     model = Word2Vec(
-        sentences=dataset,
+        # sentences=dataset,
+        corpus_file=location + "/datasets/trimmed.txt",
         vector_size=300,
         window=7,
-        min_count=5, # default 5
+        alpha=0.1,  # default 0.025
+        min_count=10,  # default 5
         sg=0,  # 0 для CBOW, 1 для Skip-gram
         epochs=100,
         compute_loss=True,
         callbacks=[TrainingProgressCallback()],
+        workers=5,
     )
     model.save(location + "/models/" + name)
     return model
@@ -60,7 +69,6 @@ def uptrain(name, epochs, dataset: MyDataset, new_name="default"):
         epochs=epochs,
         compute_loss=True,
         callbacks=[TrainingProgressCallback()],
-        # set workers in case using laptop
     )
     if new_name == "default":
         model.save(location + "/models/" + name[:-4] + "_upt.bin")
@@ -79,19 +87,22 @@ if __name__ == "__main__":
     MODEL_NAME = "cbow_v300.bin"
     DATASET_NAME = "trimmed.txt"
 
-    dataset = MyDataset(DATASET_NAME)
+    # dataset = MyDataset(DATASET_NAME)
 
-    create_model(MODEL_NAME, dataset)
+    # create_model(MODEL_NAME, dataset)
+    # create_model(MODEL_NAME)
+
     # uptrain(NAME, 60, dataset=DATASET)
 
     model = Word2Vec.load(location + "/models/" + MODEL_NAME)
 
-    def print_similar(word):
+    def print_similar(word_in):
+        word = tools.lemmatize(word_in)
         if word in model.wv:
             similar_words = model.wv.most_similar(word, topn=5)
         else:
             similar_words = []
-        print(f"{word}:", [sim_word for sim_word, _ in similar_words])
+        print(f"{word_in} = {word}:", [sim_word for sim_word, _ in similar_words])
 
     for word_to_check in "панк жизнь земля президент путин семья вода россия ящер".split():
         print_similar(word_to_check)
